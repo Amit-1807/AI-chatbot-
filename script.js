49c18740f1ad8a91b423b29ab9f80632c0e4ba16
AMIT SHARMA <150581997+Amit-1807@users.noreply.github.com>
2025-03-02 13:48:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const closeChatbot = document.querySelector("#close-chatbot");
 
 
 // API setup
-const API_KEY = "AIzaSyBCOxLG8lATJHSs_i8dGHZf009YRpmzD2s";
+const API_KEY = "AIzaSyBCOxLG8lATJHSs_i8dGHZf009YRpmzD2sAlmktsd";
 const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${API_KEY} `;
 
 const userData = {
